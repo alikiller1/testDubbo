0ca3606473a5142403c646c561249b3da72acc21
alikiller1 <liu2011321@sina.com>
2016-09-10 00:51:18 +0800

    Signed-off-by: alikiller1 <liu2011321@sina.com>

---
+++ src/main/java/per/liuqh/testdubbo/Consumer.java
@@ -0,0 +1,29 @@
+package per.liuqh.testdubbo;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;  
+  
+public class Consumer {  
+  
+    public static void main(String[] args) throws Exception {  
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(  
+                new String[] { "dubbo-consumer.xml" });  
+        context.start();  
+        System.out.println("消费都启动了");
+        DemoService demoService = (DemoService) context.getBean("demoService"); //  
+        String hello = demoService.sayHello("tom"); // ִ  
+        System.out.println(hello); //   
+  
+        //   
+        List<User> list = demoService.getUsers();  
+        if (list != null && list.size() > 0) {  
+            for (int i = 0; i < list.size(); i++) {  
+                System.out.println(list.get(i));  
+            }  
+        }  
+        // System.out.println(demoService.hehe());  
+        System.in.read();  
+    }  
+  
+}  
